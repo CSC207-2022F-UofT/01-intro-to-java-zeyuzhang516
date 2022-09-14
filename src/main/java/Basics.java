e9d89a2dc10df35d441b3bc0165504059b7fa45f
Zeyu Zhang <zeyuzh_zhang@hotmail.com>
2022-09-14 10:52:09 -0400

    Submit

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,9 +101,14 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i--){
+            String x = "Current count: " + String.valueOf(i);
+            System.out.println(x);
+        }
 
 
     }
+
 
     /**
      *  6. Below is a method named split which returns a String,
@@ -140,10 +147,13 @@ public class Basics {
          *                separated by spaces, so no loops are required, though
          *                you may use them if you wish.
          */
-        StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
-
+        StringBuilder ret = new StringBuilder();
+        String[] newString = to_split.split(" ");
+        for(int i = 0; i < newString.length; i++){
+            ret.append(newString[i].charAt(0));
+        }
         return ret.toString();
     }
 
@@ -170,7 +180,9 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
-
+        for(int i = 1; i < arr.length; i = i + 2){
+            current_sum = current_sum + arr[i];
+        }
         return current_sum;
     }
 
